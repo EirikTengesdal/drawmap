db07378a4cd8b4d7810c406d15e6caa95b085e4a
Eirik Tengesdal <82897707+EirikTengesdal@users.noreply.github.com>
2024-09-15 23:57:25 +0200

    Update browse_owsdata.R

---
+++ R/browse_wfsdata.R
@@ -0,0 +1,19 @@
+#' Browse WFS data from URL using \code{OWS4R::WFSClient()}
+#'
+#' \code{browse_wfsdata()}: Browse WFS data from a URL using
+#' \code{OWS4R::WFSClient()}.
+#'
+#' @param url URL to a service from which to retrieve data
+#' @param service_version Version of the service; it might be necessary to check
+#'   serviceVersion manually at time of request
+#'
+#' @return A data.frame (\code{pretty = TRUE}) containing available feature
+#'   types
+#' @export
+#'
+#' @seealso [load_wfsdata()], [load_maps_data()]
+browse_wfsdata <- function(url, service_version) {
+  data_client <- ows4R::WFSClient$new(url, serviceVersion = service_version)
+
+  return(data_client$getFeatureTypes(pretty = TRUE))
+}
