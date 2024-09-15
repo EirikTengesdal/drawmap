1d0b29f9721c4782976c576cee3392929e9cfd0a
Eirik Tengesdal <82897707+EirikTengesdal@users.noreply.github.com>
2024-09-15 23:58:06 +0200

    Update harmonise_vars.R

--- R/harmonise_vars.R
+++ R/harmonise_vars.R
@@ -1,23 +1,24 @@
-#' `harmonise_vars`: Harmonise variables for subsequent merging of `sf` objects
+#' Harmonise variables for subsequent merging of \code{sf} objects
 #'
-#' @param data
-#' @param rename_list
-#' @param new_var
-#' @param new_val
-#' @param position
-#' @param mutate_col
-#' @param mutate_suffix
-#' @param replace_var
-#' @param replace_val
+#' \code{harmonise_vars()}: Harmonise variables in an \code{sf} object for
+#' subsequent merging with other \code{sf} object(-s) with compatible variable
+#' names. This could be relevant for example when working with GADM map data and
+#' merging with map data from other sources.
 #'
-#' @return An `sf` object whose variables have been harmonised for subsequent merging with other sf object(-s) with compatible variable names
+#' @param data An \code{sf} object
+#' @param rename_list A named list of variables to rename
+#' @param new_var A new variable to create
+#' @param new_val A value for the new variable
+#' @param position Position of the new variable
+#' @param mutate_col A variable to mutate
+#' @param mutate_suffix A suffix to add to the mutated variable
+#' @param replace_var A variable to replace
+#' @param replace_val A value with which to replace the variable
+#'
+#' @return An \code{sf} object
+#' @importFrom dplyr %>%
+#' @importFrom rlang :=
 #' @export
-#'
-#' @examples norway <- harmonise_vars(data = norway,
-#'                                    rename_list = list("GID_0" = "rmapshaperid", "geom" = "geometry"),
-#'                                    new_var = "COUNTRY",
-#'                                    new_val = "Norway",
-#'                                    position = "GID_0")
 harmonise_vars <- function(data,
                            rename_list = NULL,
                            new_var = NULL,
@@ -27,13 +28,12 @@ harmonise_vars <- function(data,
                            mutate_suffix = NULL,
                            replace_var = NULL,
                            replace_val = NULL) {
-
-  if(!is.null(rename_list)) {
+  if (!is.null(rename_list)) {
     data <- data %>%
       dplyr::rename(!!!rename_list)
   }
 
-  if(!is.null(new_var)) {
+  if (!is.null(new_var)) {
     data <- data %>%
       dplyr::mutate(!!new_var := new_val, .after = position)
   }
@@ -43,7 +43,7 @@ harmonise_vars <- function(data,
       dplyr::mutate({{mutate_col}} := paste0({{mutate_col}}, mutate_suffix))
   }
 
-  if(!is.null(replace_var)) {
+  if (!is.null(replace_var)) {
     data <- data %>%
       dplyr::mutate(!!replace_var := replace_val)
   }
