440eb0786bd03f5bbef91abd1ec8922b9aa607da
Eirik Tengesdal <82897707+EirikTengesdal@users.noreply.github.com>
2024-09-15 23:58:50 +0200

    Update merge_sf.R

--- R/merge_sf.R
+++ R/merge_sf.R
@@ -1,43 +1,50 @@
-#' `merge_sf`: Merge `sf` objects from a specified folder
+#' Merge \code{sf} objects from a specified folder
 #'
-#' @param folder
-#' @param layer
-#' @param check_layers
-#' @param wgs84
-#' @param other_data
-#' @param ...
+#' \code{merge_sf()}: Read all Geopackage files in a specified folder and
+#' combine them into a single \code{sf} object. It is tailored for importing
+#' downloaded \href{https://gadm.org/}{GADM spatial data}.
 #'
-#' @return An `sf` object containing data from all Geopackage files in a specified folder. The function is intended to be used for the \href{https://gadm.org/}{GADM spatial data}.
+#' @param folder A character string specifying the folder containing the data
+#' @param layer A character string specifying the layer to read from the
+#'   Geopackage file
+#' @param check_layers A logical value specifying whether to return the
+#'   \code{sf::st_layers} output for each file in the folder
+#' @param wgs84 A logical value specifying whether to transform the data to WGS
+#'   84 (EPSG:4326) coördinate reference system
+#' @param other_data An optional \code{sf} object to add to the final \code{sf}
+#'   object
+#' @param ... Additional arguments to pass to \code{sf::st_read()}
+#'
+#' @return An \code{sf} object
 #' @export
 #'
 #' @examples
-#' europe <- merge_sf("data/raw")
+#' europe <- merge_sf(folder = here::here("data", "raw"))
 merge_sf <- function(folder,
                      layer = "ADM_ADM_0",
                      check_layers = FALSE,
                      wgs84 = TRUE,
                      other_data = NULL,
-                     ...){
-  # Find all Geopackage files from GADM to read in a specified folder
+                     ...) {
   files <- list.files(folder, pattern = ".gpkg", full.names = TRUE)
 
-  # If check_layers is TRUE: For each file in the list, run and return the `sf::st_layers` output
-  if(check_layers){return(lapply(files, sf::st_layers))}
+  if (check_layers) {
+    return(lapply(files, sf::st_layers))
+  }
 
-  # Read the files into separate `sf` objects
-  data <- lapply(files, function(file){
+  data <- lapply(files, function(file) {
     sf::st_read(file, layer = layer, ...)
   })
 
-  # Transform to WGS84 if wgs84 is TRUE
-  if(wgs84){data <- lapply(data, sf::st_transform, crs = "EPSG:4326")}
+  if (wgs84) {
+    data <- lapply(data, sf::st_transform, crs = "EPSG:4326")
+  }
 
-  # Combine the data into a single `sf` object
   data <- do.call(rbind, data)
 
-  # Add any external sf object as an optional function argument and do this before combining the data into a single `sf` object (must be WGS 84)
-  if(!is.null(other_data)){
+  if (!is.null(other_data)) {
     data <- rbind(data, other_data)
   }
+
   return(data)
 }
